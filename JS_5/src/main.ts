c5f735b9a9fe4ea9aca263fd377ac889f67a4d04
MeryBr <mery@bratash.net>
2024-11-01 20:48:53 +0100

    JS_5

--- JS_5/src/main.ts
+++ JS_5/src/main.ts
@@ -80,13 +80,13 @@ if (
   // Mostrar las alertas por puntuación
   function mostrarAlertaPorPuntuacion(puntuacion: number) {
     if (puntuacion < 4) {
-      alert('Has sido muy conservador.');
+      alert('Has sido muy conservador... 🐢 ¡No tengas miedo de arriesgar un poco!');
     } else if (puntuacion === 5) {
-      alert('Te ha entrado el canguelo eh?.');
+      alert('Te ha entrado el canguelo, ¿eh? 😅 ¡Vamos, atrévete un poco más!');
     } else if (puntuacion === 6 || puntuacion === 7) {
-      alert('Casi casi...');
+      alert('¡Casi casi! 🎯 ¡Estás a un paso de la gloria!');
     } else if (puntuacion === 7.5) {
-      alert('¡Lo has clavado! ¡Enhorabuena!');
+      alert('¡Lo has clavado! 🎉 ¡Perfecto, eres imbatible! 👑🥳');
     }
   }
 
