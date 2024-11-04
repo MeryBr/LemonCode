ce37617d901f244a62f2bd8574079699ba84346e
MeryBr <mery@bratash.net>
2024-11-04 23:13:55 +0100

    JS_5

--- Laboratorio Módulo 5/src/main.ts
+++ Laboratorio Módulo 5/src/main.ts
@@ -22,7 +22,7 @@ if (
     juegoActivo = true;
     cartasPedidas = [];
     puntosInfo.textContent = puntuacion.toString();
-    imgCarta.src = 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg';
+    imgCarta.src = obtenerImagenDeCarta(0); // Muestra la carta de fondo
     btnReinicio.style.display = 'none';
     btnCarta.style.display = 'block';
     btnPlantarse.style.display = 'block';
@@ -35,38 +35,48 @@ if (
   }
 
   function mostrarCarta(carta: number) {
-    const cartaImg = obtenerImagenDeCarta(carta);
-    imgCarta.src = cartaImg;
+    imgCarta.src = obtenerImagenDeCarta(carta);
   }
 
   function obtenerImagenDeCarta(carta: number): string {
-    switch (carta) {
-      case 1: return 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg';
-      case 2: return 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg';
-      case 3: return 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg';
-      case 4: return 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg';
-      case 5: return 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg';
-      case 6: return 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg';
-      case 7: return 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg';
-      case 10: return 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg';
-      case 11: return 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg';
-      case 12: return 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg';
-      default: return 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg';
-    }
+    const cartas: { [key: number]: string } = {
+      1: 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/1_as-copas.jpg',
+      2: 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/2_dos-copas.jpg',
+      3: 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/3_tres-copas.jpg',
+      4: 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/4_cuatro-copas.jpg',
+      5: 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/5_cinco-copas.jpg',
+      6: 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/6_seis-copas.jpg',
+      7: 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/7_siete-copas.jpg',
+      10: 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/10_sota-copas.jpg',
+      11: 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/11_caballo-copas.jpg',
+      12: 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/copas/12_rey-copas.jpg'
+    };
+    return cartas[carta] || 'https://raw.githubusercontent.com/Lemoncode/fotos-ejemplos/main/cartas/back.jpg';
+  }
+
+  function obtenerPuntos(carta: number): number {
+    return (carta > 7) ? 0.5 : carta;
   }
 
   function sumarPuntos(carta: number) {
-    if (carta > 7) carta = 0.5;
-    puntuacion += carta;
+    const puntosCarta = obtenerPuntos(carta);
+    puntuacion += puntosCarta;
     puntosInfo.textContent = puntuacion.toString();
+  }
+
+  function verificarPuntuacion() {
     if (puntuacion > 7.5) {
       setTimeout(() => {
         alert('Game Over! Te has pasado de 7.5 puntos.');
-        btnCarta.style.display = 'none';
-        btnPlantarse.style.display = 'none';
-        btnReinicio.style.display = 'block';
+        finalizarJuego();
       }, 100);
     }
+  }
+
+  function finalizarJuego() {
+    btnCarta.style.display = 'none';
+    btnPlantarse.style.display = 'none';
+    btnReinicio.style.display = 'block';
   }
 
   function mostrarAlertaPorPuntuacion(puntuacion: number) {
@@ -81,12 +91,44 @@ if (
     }
   }
 
+  function verPosibilidades() {
+    if (!juegoActivo) {
+      let posiblesCartas: number[] = [];
+      const totalCartas = 12;
+      while (posiblesCartas.length < 3) {
+        const cartaFutura = dameCarta();
+        if (!cartasPedidas.includes(cartaFutura) && !posiblesCartas.includes(cartaFutura)) {
+          posiblesCartas.push(cartaFutura);
+        }
+        if (posiblesCartas.length + cartasPedidas.length >= totalCartas) {
+          alert('No hay suficientes cartas nuevas disponibles para mostrar.');
+          break;
+        }
+      }
+      mostrarResultadoPosibilidades(posiblesCartas);
+    }
+  }
+
+  function mostrarResultadoPosibilidades(posiblesCartas: number[]) {
+    if (posiblesCartas.length > 0) {
+      let resultado = "Si hubieras pedido más cartas, habrías obtenido:\n";
+      posiblesCartas.forEach(carta => {
+        const valorCarta = obtenerPuntos(carta);
+        resultado += `Carta: ${carta} (Valor: ${valorCarta})\n`;
+      });
+      alert(resultado);
+    } else {
+      alert('No hay nuevas cartas disponibles para mostrar.');
+    }
+  }
+
   btnCarta.addEventListener('click', () => {
     if (juegoActivo) {
       const carta = dameCarta();
       cartasPedidas.push(carta);
       mostrarCarta(carta);
       sumarPuntos(carta);
+      verificarPuntuacion();
     }
   });
 
@@ -101,32 +143,7 @@ if (
     }
   });
 
-  btnVerPosibilidades.addEventListener('click', () => {
-    if (!juegoActivo) {
-      let posiblesCartas: number[] = [];
-      const totalCartas = 12;
-      while (posiblesCartas.length < 3) {
-        const cartaFutura = dameCarta();
-        if (!cartasPedidas.includes(cartaFutura) && !posiblesCartas.includes(cartaFutura)) {
-          posiblesCartas.push(cartaFutura);
-        }
-        if (posiblesCartas.length + cartasPedidas.length >= totalCartas) {
-          alert('No hay suficientes cartas nuevas disponibles para mostrar.');
-          break;
-        }
-      }
-      if (posiblesCartas.length > 0) {
-        let resultado = "Si hubieras pedido más cartas, habrías obtenido:\n";
-        posiblesCartas.forEach(carta => {
-          resultado += `Carta: ${carta} (Valor: ${carta > 7 ? 0.5 : carta})\n`;
-        });
-        alert(resultado);
-      } else {
-        alert('No hay nuevas cartas disponibles para mostrar.');
-      }
-    }
-  });
-
+  btnVerPosibilidades.addEventListener('click', verPosibilidades);
   btnReinicio.addEventListener('click', inicializarJuego);
   window.onload = inicializarJuego;
 }
